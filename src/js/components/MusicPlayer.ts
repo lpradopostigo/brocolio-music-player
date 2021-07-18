f72cc1e6e6b44c197d98ada232f3d6dd76cbb870
Luis Prado <lpradopostigo@gmail.com>
2021-07-18 17:37:42 -0500

    implementing redux workflow

--- src/js/components/MusicPlayer.ts
+++ src/js/components/MusicPlayer.ts
@@ -2,10 +2,12 @@ import { html, LitElement, TemplateResult } from 'lit'
 import { customElement } from 'lit/decorators.js'
 import './MiniMusicPlayer'
 import './MusicPlaylist'
-import AudioPlayer from '../services/AudioPlayer'
+import { AudioPlayer } from '../services/AudioPlayer'
 import { styles } from './MusicPlayer.styles'
-import { AudioActionType, store } from '../services/store'
+import { store } from '../services/store'
 import { Unsubscribe } from '@reduxjs/toolkit'
+import * as initActions from '../services/initActions'
+import { AudioActionType } from '../services/audioActions'
 
 @customElement('music-player')
 export default class MusicPlayer extends LitElement {
@@ -15,10 +17,13 @@ export default class MusicPlayer extends LitElement {
 
   constructor () {
     super()
+    store.dispatch(initActions.setAudioTime(
+      () => { return this.audioPlayer.currentTime },
+      () => { return this.audioPlayer.duration }))
     this.handleActionDispatched = this.handleActionDispatched.bind(this)
   }
 
-  render (): TemplateResult {
+  render (): TemplateResult<1> {
     return html`
         <mini-music-player></mini-music-player>
         <music-playlist></music-playlist>
@@ -30,12 +35,31 @@ export default class MusicPlayer extends LitElement {
     this.storeUnsubscribe = store.subscribe(this.handleActionDispatched)
   }
 
+  disconnectedCallback (): void {
+    super.disconnectedCallback()
+    if (this.storeUnsubscribe == null) {
+      throw Error('Cannot Unsubscribe')
+    }
+    this.storeUnsubscribe()
+  }
+
   handleActionDispatched (): void {
     const state = store.getState()
     switch (state.lastActionType) {
       case AudioActionType.PLAY: {
         this.audioPlayer.changeAudio(state.audioFile)
         this.audioPlayer.play()
+        break
+      }
+
+      case AudioActionType.RESUME: {
+        this.audioPlayer.play()
+        break
+      }
+
+      case AudioActionType.PAUSE: {
+        this.audioPlayer.pause()
+        break
       }
     }
   }
