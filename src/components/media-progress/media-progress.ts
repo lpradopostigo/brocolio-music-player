eb3f01f494662049015aef340dde8d2eeca8ce3b
Luis Prado <lpradopostigo@gmail.com>
2021-07-22 22:39:05 -0500

    minor refactoring

--- src/components/media-progress/media-progress.ts
+++ src/components/media-progress/media-progress.ts
@@ -6,39 +6,29 @@ import { customElement, property } from 'lit/decorators.js'
 @customElement('media-progress')
 export class MediaProgress extends LitElement {
   static styles = styles
-  @property({ type: Number })
-  value = 0
+  @property({ type: Number }) value = 0
+  @property({ type: Boolean }) disabled = false
 
-  @property({ type: Boolean })
-  disabled = false
-
-  barProgress: HTMLDivElement | null = null
+  private get barProgressWidth (): string {
+    return (this.value >= 0 && this.value <= 100) ? `${this.value}%` : '0%'
+  }
 
   render (): TemplateResult {
-    if (this.barProgress != null) {
-      this.barProgress.style.width = (this.value >= 0 && this.value <= 100) ? `${this.value}%` : '0%'
-    }
-
     return html`
         <div class="container" @click=${this.handleClick}>
             <div class="bar">
-                <div class="bar__progress"></div>
+                <div class="bar__progress"
+                     style="width: ${this.barProgressWidth}"
+                ></div>
             </div>
         </div>
     `
   }
 
-  firstUpdated (): void {
-    if (this.shadowRoot == null) {
-      throw Error('Cannot get ShadowRoot')
-    }
-    this.barProgress = this.shadowRoot.querySelector('.bar__progress')
-  }
-
-  handleClick (e: MouseEvent): void {
+  private handleClick (event: MouseEvent): void {
     if (!this.disabled) {
       const { width, x } = this.getBoundingClientRect()
-      const relX = e.x - x
+      const relX = event.x - x
       this.value = valueToPercentage(relX, width)
     }
   }
